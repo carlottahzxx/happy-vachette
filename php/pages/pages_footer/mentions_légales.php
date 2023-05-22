c97bf506ce45fad8979a936f90a33ee6bf7304fa
Maixange <120005889+Maixange@users.noreply.github.com>
2023-05-22 13:57:03 +0200

    changes

--- php/pages/pages_footer/mentions_légales.php
+++ php/pages/pages_footer/mentions_légales.php
@@ -23,9 +23,9 @@
     <h1>Mentions légales</h1>
         
 
-            <div class = "general-text">
+            <div class = "cgu-div">
             
-                <div class = "description-text">
+                <div class = "cgu-text">
                     <p>Responsable éditorial : Antoine Faust<br>
                     Siège social : 10 rue de Vanves, 92130 Issy-les-Moulineaux<br>
                     Téléphone : +33 6 51 28 47 83<br>
@@ -35,14 +35,14 @@
                     Roubaix, France<br>Téléphone : +33 9 72 10 10 07 <br>Site internet : www.ovh.com<p>
                 </div>
 
-                <div class = "description-text">
+                <div class = "cgu-text">
                     <p><br>Propriété intellectuelle : <p><p><br>Le contenu du site internet (textes, images, vidéos, etc.) est la propriété exclusive de Happy Vachette ou de ses partenaires et est protégé par les lois françaises et internationales sur la propriété intellectuelle. Toute reproduction, représentation, modification, publication, transmission, adaptation, totale ou partielle de ce contenu est strictement interdite sans l'autorisation écrite préalable de Happy Vachette.</p>
                 </div>
 
-                <div class = "description-text">
+                <div class = "cgu-text">
                     <p><br>Données personnelles :<p><p> <br>Happy Vachette s'engage à protéger la vie privée de ses utilisateurs. Les données personnelles collectées sur ce site internet sont utilisées uniquement dans le cadre des relations commerciales entre Happy Vachette et ses clients et ne sont en aucun cas communiquées à des tiers. Conformément à la loi "Informatique et Libertés" du 6 janvier 1978, vous disposez d'un droit d'accès, de rectification et de suppression de vos données personnelles en nous contactant par email à l'adresse contact@happyvachette.com.</p>
                 </div>
-                <div class = "description-text">
+                <div class = "cgu-text">
                     <p><br>Cookies : <p><p><br>Le site internet de Happy Vachette utilise des cookies pour améliorer l'expérience utilisateur et pour analyser l'audience du site. Les cookies sont de petits fichiers textes stockés sur le disque dur de l'utilisateur. Ils ne permettent pas d'identifier personnellement l'utilisateur mais enregistrent des informations relatives à la navigation sur le site internet. En utilisant ce site internet, vous acceptez l'utilisation de cookies par Happy Vachette.<p>
                     <p><br>Loi applicable : <p><p><br>Les présentes mentions légales sont soumises à la loi française. Tout litige relatif à l'utilisation du site internet de Happy Vachette sera porté devant les tribunaux compétents de Paris.<p>
 
