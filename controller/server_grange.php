b5aaee897f235dee58605be3b4dbc36891228fbe
DanielLeurquin <danielleurquin@hotmail.fr>
2023-06-05 22:17:24 +0200

    test

--- controller/server_grange.php
+++ controller/server_grange.php
@@ -95,7 +95,7 @@
 
             $query = "INSERT INTO grange (Lieu,Nom,IdUser) VALUES ('$lieu','$nom','$id_user')";
             mysqli_query($db, $query);
-            header("Location:grange.php");
+            header("Location:grange.php",true);
             exit();
 
         }
